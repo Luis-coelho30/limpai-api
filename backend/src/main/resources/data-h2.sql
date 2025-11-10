d364ca66c7ff67cdcdf66c9021fbaf75997b972f
Luis Augusto Coelho de Souza <luis.coelho.761@gmail.com>
2025-11-10 01:06:22 -0300

    Ajusta ambiente de dev e test

---
+++ backend/src/main/resources/data-h2.sql
@@ -0,0 +1,44 @@
+INSERT INTO estado (estado_id, nome, sigla) VALUES
+(1, 'Acre', 'AC'),
+(2, 'Alagoas', 'AL'),
+(3, 'Amazonas', 'AM'),
+(4, 'Amapá', 'AP'),
+(5, 'Bahia', 'BA'),
+(6, 'Ceará', 'CE'),
+(7, 'Distrito Federal', 'DF'),
+(8, 'Espírito Santo', 'ES'),
+(9, 'Goiás', 'GO'),
+(10, 'Maranhão', 'MA'),
+(11, 'Minas Gerais', 'MG'),
+(12, 'Mato Grosso do Sul', 'MS'),
+(13, 'Mato Grosso', 'MT'),
+(14, 'Pará', 'PA'),
+(15, 'Paraíba', 'PB'),
+(16, 'Pernambuco', 'PE'),
+(17, 'Piauí', 'PI'),
+(18, 'Paraná', 'PR'),
+(19, 'Rio de Janeiro', 'RJ'),
+(20, 'Rio Grande do Norte', 'RN'),
+(21, 'Rondônia', 'RO'),
+(22, 'Roraima', 'RR'),
+(23, 'Rio Grande do Sul', 'RS'),
+(24, 'Santa Catarina', 'SC'),
+(25, 'Sergipe', 'SE'),
+(26, 'São Paulo', 'SP'),
+(27, 'Tocantins', 'TO');
+
+INSERT INTO cidade (nome, estado_id) VALUES
+('Rio Branco', 1),   -- AC
+('Maceió', 2),       -- AL
+('Manaus', 3),       -- AM
+('Salvador', 5),     -- BA
+('Brasília', 7),     -- DF
+('Belo Horizonte', 11), -- MG
+('Curitiba', 18),    -- PR
+('Rio de Janeiro', 19), -- RJ
+('Niterói', 19),     -- RJ
+('Porto Alegre', 23), -- RS
+('Florianópolis', 24), -- SC
+('São Paulo', 26),   -- SP
+('Campinas', 26),    -- SP
+('Santos', 26);      -- SP
